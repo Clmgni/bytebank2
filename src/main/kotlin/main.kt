8a3b8adfda64da482ede6ca11d39392295579677
Claudio Mgni <66088320+Clmgni@users.noreply.github.com>
2021-03-05 19:47:37 -0300

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,6 @@
+fun main() {
+    testaAutenticacao()
+    
+    testaFuncionarios()
+
+}
---
+++ src/main/kotlin/modelo/FuncionarioAdmin.kt
@@ -0,0 +1,23 @@
+package modelo
+
+import modelo.Autenticavel
+import modelo.Funcionario
+
+abstract class FuncionarioAdmin(
+    nome: String,
+    cpf: String,
+    salario: Double,
+    protected val senha: Int
+) : Funcionario(
+    nome = nome,
+    cpf = cpf,
+    salario = salario
+), Autenticavel {
+
+    override fun autentica(senha: Int): Boolean {
+        if(this.senha == senha){
+            return true
+        }
+        return false
+    }
+}
