26e7446c39da9018db35d7cde5a1b3ccca46eaf3
Claudio Mgni <66088320+Clmgni@users.noreply.github.com>
2021-03-19 16:39:13 -0300

    Versao2

--- src/main/kotlin/br/com/clmgni/bytebank2/main.kt
+++ src/main/kotlin/br/com/clmgni/bytebank2/main.kt
@@ -1,6 +1,26 @@
-import br.com.clmgni.bytebank2.teste.testaAny
+import br.com.clmgni.bytebank2.util
 
 fun main() {
-    testaAny()
+
+    util.iLog()
+
     println(System.getProperty("user.language"))
+
+    util.gLog("Iniciando!", "INFO")
+
+    println(util.Companion.testeVar)
+
+    var nome = "João da silva ####"
+    var texto = util.validaNome(nome)
+    var regexC = "[A-Z][a-z].* [A-Z][a-z].*".toRegex()
+
+    if (texto.matches(regexC)) {
+        println("Nome válido: $texto")
+        util.gLog("Nome OK", "INFO")
+    } else {
+        println("Nome inválido: $texto")
+        util.gLog("Nome não OK", "ERROR")
+
+    }
+
 }
